bdf8cb324574b4a6edccdbe2f4ca55d7c79b0513
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2018-11-13 22:11:42 +0200

    Strip body-parser since it's not required for now.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import * as express from "express";
-import {Request, Response} from "express";
-import * as bodyParser from  "body-parser";
+import { Request, Response } from "express";
 
 
 const hostname = "localhost";
 const port = 3000;
 
-
 const app = express();
-app.use(bodyParser.json());
-
 
 app.get("/", function(req: Request, res: Response) {
     res.send("Hello, world!");
